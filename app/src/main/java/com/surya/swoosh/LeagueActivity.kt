fe49e3ca74b4629c0dbbd2c4005f54e8ea668ca5
surya <surya950302@gmail.com>
2020-06-19 23:50:26 -0400

    commit with league selection Logic

--- app/src/main/java/com/surya/swoosh/LeagueActivity.kt
+++ app/src/main/java/com/surya/swoosh/LeagueActivity.kt
@@ -4,14 +4,41 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_league.*
 
 class LeagueActivity : BaseActivity() {
+    var selectedLeague = ""
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_league)
     }
     fun leagueNextClicked(view : View){
         val skillIntent = Intent(this,SkillActivity :: class.java )
-        startActivity(skillIntent)
+        if(selectedLeague != ""){
+            startActivity(skillIntent)
+        }
+        else{
+            Toast.makeText(this, "Please select a league", Toast.LENGTH_SHORT).show()
+        }
+
+    }
+    fun mensOnClicked(view : View){
+        womensButton.isChecked = false
+        coedButton.isChecked = false
+
+        selectedLeague = "mens"
+    }
+    fun womensOnClicked(view : View){
+        mensButton.isChecked = false
+        coedButton.isChecked = false
+
+        selectedLeague = "womens"
+    }
+    fun coedOnclicked(view : View){
+        mensButton.isChecked = false
+        womensButton.isChecked = false
+
+        selectedLeague = "coed"
     }
 }
